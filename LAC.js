5cd895ade5ee567ee1e164ebb8b78ddf7a241af1
newline-CRLF <szjaym@gmail.com>
2025-05-07 23:53:01 +0900

    修正

--- LAC.js
+++ LAC.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const targetDiv = document.getElementById('accessCounter');
         if (targetDiv) {
             targetDiv.innerHTML = `
-                <img src="https://count.getloli.com/@CR-LF?name=CR-LF&theme=gelbooru&padding=8&offset=0&align=top&scale=1&pixelated=1&darkmode=auto" class="center" alt="アクセスカウンター" width="33%">
+                <img src="https://count.getloli.com/@CR-LF?name=CR-LF&theme=gelbooru&padding=8&offset=0&align=top&scale=1&pixelated=1&darkmode=auto" class="center" alt="アクセスカウンター" width="50%">
                 <p class="gray center">↑アクセスカウンター</p>
             `;
         }
