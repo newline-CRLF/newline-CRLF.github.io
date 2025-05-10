b8ff730707cad21a01b3f4749a616b3840920537
newline-CRLF <szjaym@gmail.com>
2025-05-10 14:41:05 +0900

    修正

--- LAC.js
+++ LAC.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (targetDiv) {
             targetDiv.innerHTML = `
                 <img src="https://count.getloli.com/@CR-LF?name=CR-LF&theme=gelbooru&padding=8&offset=0&align=top&scale=1&pixelated=1&darkmode=auto" class="center" alt="アクセスカウンター" width="50%">
-                <p class="gray center">↑アクセスカウンター</p>
+                <p class="gray center">↑アクセスカウンター(無断転載禁止)</p>
             `;
         }
     }
